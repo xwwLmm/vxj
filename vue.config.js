8b20dbb0bd9711cae8d7e8e0fdc5528d4e8c383c
xieww <xieww@raiyee.com>
2019-03-12 17:37:56 +0800

    fix: code split

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,38 @@
 var path = require('path')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+
+const PUBLISH = process.env.TARGET === 'lib'
+const PROD = process.env.NODE_ENV === 'production'
+const plugins = []
+
+if (PROD) {
+  plugins.push(new UglifyJsPlugin({
+    uglifyOptions: {
+      test: /\.js(\?.*)?$/i,
+      compress: {
+        warnings: false
+      }
+    },
+    sourceMap: false,
+    parallel: true
+  }))
+}
+
+const optimization = !PUBLISH ? {
+  splitChunks: {
+    cacheGroups: {
+      vendor: {
+        chunks: 'all',
+        test: /[\\/]node_modules[\\/](vue|vuex|vue-router|vxj)/,
+        minChunks: 1,
+        name: 'vendor'
+      }
+    }
+  },
+  runtimeChunk: {
+    name: 'manifest',
+  }
+} : {}
 
 module.exports = {
   css: {
@@ -10,20 +44,7 @@ module.exports = {
   },
   publicPath: process.env.NODE_ENV === 'production' ? '/vxj' : './',
   configureWebpack: {
-    optimization: {
-      splitChunks: {
-        cacheGroups: {
-          vendor: {
-            chunks: 'all',
-            test: /[\\/]node_modules[\\/](vue|vuex|vue-router|vxj)/,
-            minChunks: 1,
-            name: 'vendor'
-          }
-        }
-      },
-      runtimeChunk: {
-        name: 'manifest',
-      }
-    }
+    plugins,
+    optimization
   }
 }
